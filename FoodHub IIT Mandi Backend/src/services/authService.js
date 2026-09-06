56a72dd48fbe7d85727a8ef9c201e264843efd6b
Sahil Sheoran <sahilsheoran34741@email.com>
2026-09-06 23:51:23 +0530

    fix: replace native bcrypt for Vercel deployment

--- FoodHub IIT Mandi Backend/src/services/authService.js
+++ FoodHub IIT Mandi Backend/src/services/authService.js
@@ -1,5 +1,5 @@
 const { findUser } = require("../repositories/userRepository");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, JWT_EXPIRY } = require("../config/serverConfig");
 
